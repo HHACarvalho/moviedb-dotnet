d337977254e1d107b365b7a24b40a9a89c6091f5
Hugo Carvalho <hhac@protonmail.com>
2023-08-02 03:10:24 +0100

    Created the class Result to allow the controller to know if the operation was a success

---
+++ moviedb-dotnet/Core/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace moviedb_dotnet.Core
+{
+    public class Result<T> where T : class
+    {
+        public bool IsSuccess { get; }
+        public string? Error { get; }
+        public T? Value { get; }
+
+        public Result(bool isSuccess, string? error, T? value)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+            Value = value;
+        }
+
+        public static Result<T> Ok(T value)
+        {
+            return new Result<T>(true, null, value);
+        }
+
+        public static Result<T> Fail(string error)
+        {
+            return new Result<T>(false, error, null);
+        }
+    }
+}
