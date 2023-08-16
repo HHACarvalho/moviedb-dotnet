f2ac830881c66ac7db2e9f8123e9cef09bba95ca
Hugo Carvalho <hhac@protonmail.com>
2023-08-16 20:07:57 +0100

    Created a DTO class to specify the format in which to return movies to the user

---
+++ moviedb-dotnet/DTOs/MovieDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace moviedb_dotnet.DTOs
+{
+    public class MovieDTO
+    {
+        public string? Title { get; set; }
+        public string? Director { get; set; }
+        public int Year { get; set; }
+    }
+}
