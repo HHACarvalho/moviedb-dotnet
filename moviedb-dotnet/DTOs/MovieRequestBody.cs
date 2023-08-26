f0c85f608153f0202090647ed8bd66a050056d71
Hugo Carvalho <hhac@protonmail.com>
2023-08-26 16:16:33 +0100

    Added incoming request body validation

--- moviedb-dotnet/DTOs/MovieRequestBody.cs
+++ moviedb-dotnet/DTOs/MovieRequestBody.cs
@@ -1,9 +1,19 @@
-﻿namespace moviedb_dotnet.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moviedb_dotnet.DTOs
 {
     public class MovieRequestBody
     {
+        [Required]
+        [StringLength(96, MinimumLength = 2)]
         public required string Title { get; set; }
+
+        [Required]
+        [StringLength(32, MinimumLength = 2)]
         public required string Director { get; set; }
-        public required int Year { get; set; }
+
+        [Required]
+        [Range(1888, int.MaxValue)]
+        public int Year { get; set; }
     }
 }
